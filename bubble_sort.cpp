f00a53da45776c106e0caf8ad864db593f80fbae
unknown <malaykumar089@gmail.com>
2024-09-16 21:10:26 +0530

    updated the bubble sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,36 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(int arr[], int n)
+void bubble_sort(int arr[], int len)
 {
-    int i, j;
-    bool swapped;
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+    int swapped = 0;
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = 0; j < len - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
-                swapped = true;
+                swapped = 1;
             }
         }
-        if (swapped == false)
+        if (swapped == 0)
             break;
     }
 }
 
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        cout << " " << arr[i];
-}
-
 int main()
 {
-    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-    int N = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, N);
-    cout << "Sorted array: \n";
-    printArray(arr, N);
+
+    int arr[] = {9, 6, 0, 5, 0, 8, 2, 4, 7};
+    int end = sizeof(arr) / sizeof(arr[0]);
+    bubble_sort(arr, end);
+    for (int i = 0; i < end; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
