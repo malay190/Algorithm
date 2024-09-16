88a3481f75c71b2c24fd22971179caa36aae85dd
unknown <malaykumar089@gmail.com>
2024-09-16 20:54:08 +0530

    updated the insertion sort

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,36 +1,31 @@
-#include<iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-void insertionSort(int arr[], int n)
+void insertion_sort(int arr[], int len)
 {
-    for (int i = 1; i < n; ++i) {
-        int key = arr[i];
+    for (int i = 1; i < len; i++)
+    {
         int j = i - 1;
-
-        while (j >= 0 && arr[j] > key) {
+        int key = arr[i];
+        while (j >= 0 && arr[j] > key)
+        {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
         arr[j + 1] = key;
     }
 }
 
-void printArray(int arr[], int n)
-{
-    for (int i = 0; i < n; ++i)
-        cout << arr[i] << " ";
-    cout << endl;
-}
-
-// Driver method
 int main()
 {
-    int arr[] = { 12, 11, 13, 5, 6 };
-    int n = sizeof(arr) / sizeof(arr[0]);
 
-    insertionSort(arr, n);
-    printArray(arr, n);
-
+    int arr[] = {9, 6, 0, 5, 0, 8, 2, 4, 7};
+    int end = sizeof(arr) / sizeof(arr[0]) - 1;
+    insertion_sort(arr, end);
+    for (int i = 0; i <= end; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
