e008fe4cd742a5a13ae34c1e0025f1fed936bb51
unknown <malaykumar089@gmail.com>
2024-09-16 19:59:30 +0530

    use the different approach

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,46 +1,69 @@
-void merge(int a[], int beg, int mid, int end)    
-{    
-    int i, j, k;  
-    int n1 = mid - beg + 1;    
-    int n2 = end - mid;    
-      
-    int LeftArray[n1], RightArray[n2];  
-      
-     
-    for (int i = 0; i < n1; i++)    
-    LeftArray[i] = a[beg + i];    
-    for (int j = 0; j < n2; j++)    
-    RightArray[j] = a[mid + 1 + j];    
-      
-    i = 0, 
-    j = 0;   
-    k = beg;  
-      
-    while (i < n1 && j < n2)    
-    {    
-        if(LeftArray[i] <= RightArray[j])    
-        {    
-            a[k] = LeftArray[i];    
-            i++;    
-        }    
-        else    
-        {    
-            a[k] = RightArray[j];    
-            j++;    
-        }    
-        k++;    
-    }    
-    while (i<n1)    
-    {    
-        a[k] = LeftArray[i];    
-        i++;    
-        k++;    
-    }    
-      
-    while (j<n2)    
-    {    
-        a[k] = RightArray[j];    
-        j++;    
-        k++;    
-    }    
-} 
+#include <bits/stdc++.h>
+using namespace std;
+
+// Important part in the merge sort is the merge algorithm, that will merge the two array and merge sort is a
+// outplace algorithm
+void merge(int arr[], int first, int mid, int last)
+{
+    int n1 = mid - first + 1;
+    int n2 = last - mid;
+
+    int *arr1 = new int[n1];
+    int *arr2 = new int[n2];
+
+    for (int i = 0; i < n1; i++)
+    {
+        arr1[i] = arr[first + i];
+    }
+    arr1[n1] = INT_MAX;
+
+    for (int j = 0; j < n2; j++)
+    {
+        arr2[j] = arr[mid + 1 + j];
+    }
+    arr2[n2] = INT_MAX;
+
+    int i = 0;
+    int j = 0;
+
+    for (int k = first; k <= last; k++)
+    {
+        if (arr1[i] > arr2[j])
+        {
+            arr[k] = arr2[j];
+            j++;
+        }
+        else
+        {
+            arr[k] = arr1[i];
+            i++;
+        }
+    }
+    delete[] arr1;
+    delete[] arr2;
+}
+
+void merge_sort(int arr[], int first, int last)
+{
+    if (first < last)
+    {
+        int q = floor((first + last) / 2);
+        merge_sort(arr, first, q);
+        merge_sort(arr, q + 1, last);
+        merge(arr, first, q, last);
+    }
+}
+int main()
+{
+
+    int arr[] = {9, 6, 0, 5, 0, 8, 2, 4, 7};
+    int start = 0;
+    int end = sizeof(arr) / sizeof(arr[0]) - 1;
+    merge_sort(arr, start, end);
+    for (int i = 0; i <= end; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
